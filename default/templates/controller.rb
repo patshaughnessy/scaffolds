cfc372a9abcc848d56f97744640ae5cf8297a595
Pat Shaughnessy <pat@patshaughnessy.net>
2011-03-24 12:18:09 -0400

    removed autocomplete code in default scaffold

--- default/templates/controller.rb
+++ default/templates/controller.rb
@@ -1,19 +1,12 @@
 class <%= controller_class_name %>Controller < ApplicationController
   # GET <%= route_url %>
   # GET <%= route_url %>.xml
-  # GET <%= route_url %>.json
   def index
-    query = params[:term]
-    if query.blank?
-      @<%= plural_table_name %> = <%= orm_class.all(class_name) %>
-    else
-      @<%= plural_table_name %> = <%= class_name %>.where("<%= scaffold_parameter %> LIKE ?", "#{query}%").limit(10)
-    end
+    @<%= plural_table_name %> = <%= orm_class.all(class_name) %>
 
     respond_to do |format|
       format.html # index.html.erb
       format.xml  { render :xml => @<%= plural_table_name %> }
-      format.json { render :text => @<%= plural_table_name %>.collect {|<%= singular_table_name %>| <%= singular_table_name %>.auto_complete_info('<%= scaffold_parameter %>')}.to_json }
     end
   end
 
--- default/templates/model.rb
+++ default/templates/model.rb
@@ -2,7 +2,4 @@ class <%= class_name %> < <%= parent_class_name.classify %>
 <% attributes.select {|attr| attr.reference? }.each do |attribute| -%>
   belongs_to :<%= attribute.name %>
 <% end -%>
-  def auto_complete_info(field)
-    { "id" => id, "label" => send(field), "value" => send(field) }
-  end
 end
