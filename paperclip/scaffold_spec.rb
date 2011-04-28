95b66b244f0710b62d5ce628ef7a74f75b3827c4
Pat Shaughnessy <pat@patshaughnessy.net>
2011-04-28 15:38:27 -0400

    updated paperclip dsl

--- paperclip/scaffold_spec.rb
+++ paperclip/scaffold_spec.rb
@@ -1,15 +1,48 @@
-Scaffoldhub::Specification.new do |s|
+Scaffoldhub::Specification.new do
 
-  s.base_url 'https://github.com/patshaughnessy/scaffolds/raw/master/paperclip'
+  # Github URL where you will post your scaffold - the speciied folder must contain this file
+  base_url  'http://localhost/github/paperclip'
 
-  s.active_record_file :src => 'templates/model.rb'
-  s.active_record_file :src => 'templates/migration.rb'
+  # The name of your new scaffold: should be a single word
+  name 'paperclip'
 
-  s.controller_file :src => 'templates/controller.rb'
+  # Metadata about this scaffold - this info is only used for display on scaffoldhub.org:
+  metadata do
 
-  s.erb_file :src => 'templates/_form.html.erb'
-  s.erb_file :src => 'templates/edit.html.erb'
-  s.erb_file :src => 'templates/index.html.erb'
-  s.erb_file :src => 'templates/new.html.erb'
-  s.erb_file :src => 'templates/show.html.erb'
+    # A short paragraph describing what this scaffold does
+    description 'The paperclip scaffold.'
+
+    # 4x3 aspect ratio screen shot
+    screenshot 'paperclip_screenshot.png'
+
+    # Tag(s) to help scaffoldhub.org users find your scaffold
+    tag 'paperclip'
+
+    # Optionally specify an example of a scaffold parameter
+    parameter_example 'ATTACHMENT_NAME'
+  end
+
+  # Optionally post a link to an article you write explaining how the scaffold works.
+  blog_post 'http://patshaughnessy.net/2011/3/13/view-mapper-for-rails-3-scaffoldhub'
+
+  # Define a model template - this ERB file will be used to generate a new
+  # model class with this path & filename: app/models/NAME.rb
+  model 'templates/model.rb'
+
+  # Define an ActiveRecord migration template - this ERB file will be used to generate a new
+  # migration class with this path & filename: db/migrate/TIMESTAMP_create_PLURAL_NAME.rb
+  migration 'templates/migration.rb'
+
+  # Define a controller template - this ERB file will be used to generate a new
+  # controller class with this path & filename: app/controllers/PLURAL_NAME.rb
+  controller 'templates/controller.rb'
+
+  # You can use "with_options" to specify the same source folder for a series of templates:
+  with_options :src => 'templates' do
+    view '_form.html.erb'
+    view 'new.html.erb'
+    view 'edit.html.erb'
+    view 'index.html.erb'
+    view 'show.html.erb'
+  end
 end
