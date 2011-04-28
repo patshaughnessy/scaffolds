a732a03b2f028c1b7e2953e69980db072c57e2bd
Pat Shaughnessy <pat@patshaughnessy.net>
2011-04-28 15:52:50 -0400

    updated autocomplete dsl

--- autocomplete/scaffold_spec.rb
+++ autocomplete/scaffold_spec.rb
@@ -1,33 +1,74 @@
-Scaffoldhub::Specification.new do |s|
+Scaffoldhub::Specification.new do
 
-  s.base_url 'https://github.com/patshaughnessy/scaffolds/raw/master/autocomplete'
+  # Github URL where you will post your scaffold - the speciied folder must contain this file
+  base_url  'http://localhost/github/autocomplete'
 
-  s.active_record_file :src => 'templates/model.rb'
+  # The name of your new scaffold: should be a single word
+  name 'autocomplete'
 
-  s.controller_file :src => 'templates/controller.rb'
+  # Metadata about this scaffold - this info is only used for display on scaffoldhub.org:
+  metadata do
 
-  s.erb_file :src => 'templates/_form.html.erb'
-  s.erb_file :src => 'templates/edit.html.erb'
-  s.erb_file :src => 'templates/index.html.erb'
-  s.erb_file :src => 'templates/new.html.erb'
-  s.erb_file :src => 'templates/show.html.erb'
-  s.erb_file :src => 'templates/layout.erb',      :dest => 'app/views/layouts'
+    # A short paragraph describing what this scaffold does
+    description 'The autocomplete scaffold.'
 
-  s.other_file :src => 'templates/jquery/jquery-1.4.4.min.js',            :dest => 'public/javascripts'
-  s.other_file :src => 'templates/jquery/jquery-ui-1.8.10.custom.min.js', :dest => 'public/javascripts'
-  s.other_file :src => 'templates/jquery/ui-lightness/jquery-ui-1.8.10.custom.css', :dest => 'public/stylesheets/ui-lightness'
-  s.other_file :src => 'templates/jquery/ui-lightness/images/ui-bg_diagonals-thick_18_b81900_40x40.png', :dest => 'public/stylesheets/ui-lightness/images'
-  s.other_file :src => 'templates/jquery/ui-lightness/images/ui-bg_diagonals-thick_20_666666_40x40.png', :dest => 'public/stylesheets/ui-lightness/images'
-  s.other_file :src => 'templates/jquery/ui-lightness/images/ui-bg_flat_10_000000_40x100.png', :dest => 'public/stylesheets/ui-lightness/images'
-  s.other_file :src => 'templates/jquery/ui-lightness/images/ui-bg_glass_100_f6f6f6_1x400.png', :dest => 'public/stylesheets/ui-lightness/images'
-  s.other_file :src => 'templates/jquery/ui-lightness/images/ui-bg_glass_100_fdf5ce_1x400.png', :dest => 'public/stylesheets/ui-lightness/images'
-  s.other_file :src => 'templates/jquery/ui-lightness/images/ui-bg_glass_65_ffffff_1x400.png', :dest => 'public/stylesheets/ui-lightness/images'
-  s.other_file :src => 'templates/jquery/ui-lightness/images/ui-bg_gloss-wave_35_f6a828_500x100.png', :dest => 'public/stylesheets/ui-lightness/images'
-  s.other_file :src => 'templates/jquery/ui-lightness/images/ui-bg_highlight-soft_100_eeeeee_1x100.png', :dest => 'public/stylesheets/ui-lightness/images'
-  s.other_file :src => 'templates/jquery/ui-lightness/images/ui-bg_highlight-soft_75_ffe45c_1x100.png', :dest => 'public/stylesheets/ui-lightness/images'
-  s.other_file :src => 'templates/jquery/ui-lightness/images/ui-icons_222222_256x240.png', :dest => 'public/stylesheets/ui-lightness/images'
-  s.other_file :src => 'templates/jquery/ui-lightness/images/ui-icons_228ef1_256x240.png', :dest => 'public/stylesheets/ui-lightness/images'
-  s.other_file :src => 'templates/jquery/ui-lightness/images/ui-icons_ef8c08_256x240.png', :dest => 'public/stylesheets/ui-lightness/images'
-  s.other_file :src => 'templates/jquery/ui-lightness/images/ui-icons_ffd27a_256x240.png', :dest => 'public/stylesheets/ui-lightness/images'
-  s.other_file :src => 'templates/jquery/ui-lightness/images/ui-icons_ffffff_256x240.png', :dest => 'public/stylesheets/ui-lightness/images'
+    # 4x3 aspect ratio screen shot
+    screenshot 'autocomplete_screenshot.png'
+
+    # Tag(s) to help scaffoldhub.org users find your scaffold
+    tag 'jquery'
+    tag 'autocomplete'
+
+    # Optionally specify an example of a scaffold parameter
+    parameter_example 'FIELD_NAME'
+  end
+
+  # Optionally post a link to an article you write explaining how the scaffold works.
+  blog_post 'http://patshaughnessy.net/2011/3/13/view-mapper-for-rails-3-scaffoldhub'
+
+  # Define a model template - this ERB file will be used to generate a new
+  # model class with this path & filename: app/models/NAME.rb
+  model 'templates/model.rb'
+
+  # Define an ActiveRecord migration template - this ERB file will be used to generate a new
+  # migration class with this path & filename: db/migrate/TIMESTAMP_create_PLURAL_NAME.rb
+  migration 'templates/migration.rb'
+
+  # Define a controller template - this ERB file will be used to generate a new
+  # controller class with this path & filename: app/controllers/PLURAL_NAME.rb
+  controller 'templates/controller.rb'
+
+  # You can use "with_options" to specify the same source folder for a series of templates:
+  with_options :src => 'templates' do
+    view '_form.html.erb'
+    view 'new.html.erb'
+    view 'edit.html.erb'
+    view 'index.html.erb'
+    view 'show.html.erb'
+    view 'layout.erb', :dest => 'app/views/layouts'
+  end
+
+  with_options :src => 'templates/jquery', :dest => 'public/javascripts' do
+    file 'jquery-1.4.4.min.js'
+    file 'jquery-ui-1.8.10.custom.min.js'
+  end
+
+  file 'templates/jquery/ui-lightness/jquery-ui-1.8.10.custom.css', :dest => 'public/stylesheets/ui-lightness'
+
+  with_options :src => 'templates/jquery/ui-lightness/images/', :dest => 'public/stylesheets/ui-lightness/images' do
+    'ui-bg_diagonals-thick_18_b81900_40x40.png'
+    'ui-bg_diagonals-thick_20_666666_40x40.png'
+    'ui-bg_flat_10_000000_40x100.png'
+    'ui-bg_glass_100_f6f6f6_1x400.png'
+    'ui-bg_glass_100_fdf5ce_1x400.png'
+    'ui-bg_glass_65_ffffff_1x400.png'
+    'ui-bg_gloss-wave_35_f6a828_500x100.png'
+    'ui-bg_highlight-soft_100_eeeeee_1x100.png'
+    'ui-bg_highlight-soft_75_ffe45c_1x100.png'
+    'ui-icons_222222_256x240.png'
+    'ui-icons_228ef1_256x240.png'
+    'ui-icons_ef8c08_256x240.png'
+    'ui-icons_ffd27a_256x240.png'
+    'ui-icons_ffffff_256x240.png'
+  end
 end
