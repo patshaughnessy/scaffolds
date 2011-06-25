d6da8fd8c97584372b90c8e3feb70b273a481daa
Pat Shaughnessy <pat@patshaughnessy.net>
2011-06-24 21:21:30 -0400

    include_root_in_json

--- cloud_edit/templates/model.rb
+++ cloud_edit/templates/model.rb
@@ -1,6 +1,8 @@
-<% attribs = attributes.collect {|attribute| ":#{attribute.name}" }.join(", ") %>
 class <%= class_name %> < ActiveRecord::Base
+  <% attribs = attributes.collect {|attribute| ":#{attribute.name}" }.join(", ") %>
   attr_accessible <%= attribs %>
+
+  self.include_root_in_json = false
 
   def to_json(options = {})
     super(options.merge(:only => [ :id, :created_at, <%= attribs %> ]))
